ed00f488bafce5f528a23512af99b8b87c4d6fff
JAOR SOFTWARE <114450795+jaorsoftware1965@users.noreply.github.com>
2023-02-26 13:48:40 -0600

    Class 001 Introduction
    1.- Directive #include
    2.- Name Spaces
    3.- Main Function
    4.- Code Block
    5.- Reserved Words
    6.- Comments
    7.- Creating a Class
    8.- Creating an Object of the Class

---
+++ class001.cpp
@@ -0,0 +1,37 @@
+// Course of Cpp
+// Class001.cpp
+// Introducciön
+
+// Creating Course Project
+// The function main
+// Create the First Class
+// Crating an object of the class
+// Reserved Words
+// directive #include
+// Creating an Object from Class001
+// Comentarios
+// NameSpaces
+
+#include "class001.h"
+
+// Library
+#include <iostream>
+
+// namespace
+using namespace std;
+
+Class001::Class001()
+{
+   cout << "Course Cpp" << endl;
+   cout << "Class 0001" << endl << endl;
+   cout << "Introduction" << endl << endl;
+
+   cout << "1.- Directive #include" << endl;
+   cout << "2.- Name Spaces" << endl;
+   cout << "3.- Main Function" << endl;
+   cout << "4.- Code Block" << endl;
+   cout << "5.- Reserved Words" << endl;
+   cout << "6.- Comments" << endl;
+   cout << "7.- Creating a Class" << endl;
+   cout << "8.- Creating an Object of the Class" << endl << endl;
+}
---
+++ class001.h
@@ -0,0 +1,11 @@
+#ifndef CLASS001_H
+#define CLASS001_H
+
+
+class Class001
+{
+public:
+    Class001();
+};
+
+#endif // CLASS001_H
